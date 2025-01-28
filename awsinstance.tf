f4c1796276571e2c7080b0c338e9764dfeac30b6
Support <support@Mourya.local>
2025-01-28 16:21:59 +0530

    AWS instance creation terraform file

---
+++ awsinstance.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+        version = "~> 4.0"
+        }
+     }
+  
+
+required_version = "1.10.5"
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+ }
+
+resource "aws_instance" "Target_Server_Ansible3" {
+    ami           = "ami-04b4f1a9cf54c11d0"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+}
