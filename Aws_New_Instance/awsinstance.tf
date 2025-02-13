1a97a4b025cfc6dcc57b2d81203998b81e600f06
Support <support@Mourya.local>
2025-02-13 17:13:08 +0530

    Instance update to t2.medium

--- Aws_New_Instance/awsinstance.tf
+++ Aws_New_Instance/awsinstance.tf
@@ -17,5 +17,5 @@ provider "aws" {
 
 resource "aws_instance" "Target_Server_Ansible3" {
     ami           = "ami-04b4f1a9cf54c11d0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+    instance_type = "t2.medium"
 }
